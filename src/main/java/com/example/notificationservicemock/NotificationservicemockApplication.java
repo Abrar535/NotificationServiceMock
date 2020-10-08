b831424a894e270a99c628748da22aededd39e23
Abrar <abrar.haq535@gmail.com>
2020-10-08 16:37:13 +0600

    Docker file commit

--- src/main/java/com/example/notificationservicemock/NotificationservicemockApplication.java
+++ src/main/java/com/example/notificationservicemock/NotificationservicemockApplication.java
@@ -1,15 +1,26 @@
 package com.example.notificationservicemock;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class NotificationservicemockApplication {
-
+@Slf4j
+public class NotificationservicemockApplication implements CommandLineRunner {
+    @Value("${spring.datasource.url}")
+    public String dbUrl;
     public static void main(String[] args) {
         SpringApplication.run(NotificationservicemockApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+      log.info("dbUrl is "+ dbUrl);
+
+
+    }
 }
